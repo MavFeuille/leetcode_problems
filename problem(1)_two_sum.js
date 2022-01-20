531c2146690764acc2d47df750842765b7b213de
MavFeuille <mavis.feuille@gmail.com>
2022-01-19 22:34:27 -0500

    Add test case

--- problem(1)_two_sum.js
+++ problem(1)_two_sum.js
@@ -1,3 +1,9 @@
 const twoSum = (nums, target) => {
-  
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] + nums[i + 1] === target) {
+      console.log(nums.indexof(i+1, i))
+    }
+  }
 }
+
+console.log(twoSum([2,7,11,15]),9)
