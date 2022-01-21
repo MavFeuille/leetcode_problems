d376daa5b3f192d7f260730fbd706b3f5f69a1c0
MavFeuille <mavis.feuille@gmail.com>
2022-01-20 21:59:12 -0500

    Set up placeholder for split string and add test case

--- problem3_longest_substring_without_repeating_characters.js
+++ problem3_longest_substring_without_repeating_characters.js
@@ -1,3 +1,6 @@
 const lengthOfLongestSubString = (s) => {
-  
+  let split = s.split('');
+  console.log("🚀 ~ file: problem3_longest_substring_without_repeating_characters.js ~ line 3 ~ lengthOfLongestSubString ~ split", split)
 }
+
+console.log(lengthOfLongestSubString("abcabcbb"));
