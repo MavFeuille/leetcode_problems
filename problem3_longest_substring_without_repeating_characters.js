e5e1e9c8a54c1a4bee0b1030131bfdf28e85bc3e
MavFeuille <mavis.feuille@gmail.com>
2022-01-20 22:02:02 -0500

    Set up for loop to loop through split string

--- problem3_longest_substring_without_repeating_characters.js
+++ problem3_longest_substring_without_repeating_characters.js
@@ -1,6 +1,14 @@
 const lengthOfLongestSubString = (s) => {
   let split = s.split('');
-  console.log("🚀 ~ file: problem3_longest_substring_without_repeating_characters.js ~ line 3 ~ lengthOfLongestSubString ~ split", split)
+  // console.log("🚀 ~ file: problem3_longest_substring_without_repeating_characters.js ~ line 3 ~ lengthOfLongestSubString ~ split", split)
+  let newArr = [];
+  for (let i = 0; i < split.length; i++) {
+    if (split[i] !== split[i + 1]) {
+      newArr.push(split[i])
+    }
+    console.log("newArr: ", newArr);
+  }
+
 }
 
 console.log(lengthOfLongestSubString("abcabcbb"));
